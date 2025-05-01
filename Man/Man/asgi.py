84a562e7ced72ec09173a2c32ac18d1843998481
Nithees95 <nitheesh71036@gmail.com>
2025-05-01 15:47:10 +0530

    Success

--- Man/Man/asgi.py
+++ Man/Man/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Nitheesh project.
+ASGI config for Man project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nitheesh.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Man.settings')
 
 application = get_asgi_application()
--- Man/Man/wsgi.py
+++ Man/Man/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Nitheesh project.
+WSGI config for Man project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nitheesh.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Man.settings')
 
 application = get_wsgi_application()
